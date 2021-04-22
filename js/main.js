6d634fde9921aa70b6ef23f6fe619ebfacc3e515
sosouni14 <82301984+sosouni14@users.noreply.github.com>
2021-04-22 23:04:51 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function showmain() {
+    document.getElementById("fig").src = "./img/main.jpg";
+    document.getElementById("desc").innerHTML = "이름: 송소연<br>학번: 91810521";
+}
+function imgload() {
+    document.getElementById("g1").src = "./img/page.jpg";
+}
+function showau() {
+    document.getElementById("fig1").src = "./img/au.jpg";
+}
